4aa402f47c199832171b5d87315ffff55cd9c4c6
abie <abie@riseup.net>
2010-08-25 22:14:19 -0700

    add smoothing to plots, and also plot residuals

--- temp_proxy.py
+++ temp_proxy.py
@@ -35,16 +35,32 @@ def y(value=data.y, mu=mu, sigma=sigma):
 vars = [beta, sigma, mu, predicted, y]
 mc = MCMC(vars)
 mc.use_step_method(Metropolis, beta)
-mc.sample(iter=500000, thin=100, burn=250000, verbose=1) # I like to burn-in for 50% of the total samples
+mc.sample(iter=20000, thin=10, burn=10000, verbose=1) # I like to burn-in for 50% of the total samples
 
 
-# plot results
-clf()
+# plotting setup
 t = range(1999, 1850, -1)
 quantiles = predicted.stats()['quantiles']
-plot(t, quantiles[50], color='black', label='Smoothed Estimate')
-plot(t, quantiles[2.5], color='grey', linestyle='dashed', label='Smoothed 90% Quantiles')
-plot(t, quantiles[97.5], color='grey', linestyle='dashed')
+
+def smooth(x):
+    from pymc import gp
+    M = gp.Mean(lambda x: zeros(len(x)))
+    C = gp.Covariance(gp.matern.euclidean, amp=1, scale=15, diff_degree=2)
+    gp.observe(M, C, range(len(x)), x, .5)
+    return M(range(len(x)))
+
+# plot results
+figure()
+plot(t, smooth(quantiles[50]), color='black', label='Smoothed Estimate')
+plot(t, smooth(quantiles[2.5]), color='grey', linestyle='dashed', label='Smoothed 90% Quantiles')
+plot(t, smooth(quantiles[97.5]), color='grey', linestyle='dashed')
+plot(t, quantiles[50], color='blue', alpha=.5, linewidth=3, label='Unsmoothed Estimate')
 plot(t, data.y, color='red', label='HADCRU NH Data')
-axis([1850, 1999, -1, 1])
-legend()
+axis([1845, 2005, -1, 1])
+legend(loc='upper left')
+
+# scatter residuals
+figure()
+scatter(data.y, quantiles[50]-data.y)
+xlabel('temp_t')
+ylabel('residual_t')
