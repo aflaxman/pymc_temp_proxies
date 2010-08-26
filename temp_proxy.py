979f26431953753e9c987f358104619e70b95168
abie <abie@riseup.net>
2010-08-25 19:20:37 -0700

    initial commit

---
+++ temp_proxy.py
@@ -0,0 +1,6 @@
+""" Script to replicate some of
+http://probabilitynotes.wordpress.com/2010/08/22/global-temperature-proxy-reconstructions-bayesian-extrapolation-of-warming-w-rjags/
+in pymc"""
+
+from pylab import *
+from pymc import *
